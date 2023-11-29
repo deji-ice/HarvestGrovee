4fe64464c483511ae87f89224d247e2d67ddd52f
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2023-11-29 16:01:16 +0100

    Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import HeroSection from './components/HeroSection'
 import NavBar from './components/NavBar'
 
 function App() {
@@ -6,6 +7,8 @@ function App() {
   return (
     <>
     <NavBar />
+    <HeroSection/>
+    {/* <div>Icons made from <a href="https://www.onlinewebfonts.com/icon">svg icons</a>is licensed by CC BY 4.0</div> */}
     </>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,19 @@
-import { CircleArrow, Logo, LogoSm } from "../assets"
+import { useState } from "react"
+import { CircleArrow, Hamburger, Logo, LogoSm } from "../assets"
 
 const NavBar = () => {
+
+    const [show, setShow] = useState(false)
+
+
     return (
-        <div className="flex justify-between px-16 py-3 items-center">
+        <div className="md:border-b fixed w-screen px-4 flex justify-between xl:px-14 py-3 items-center">
             <div className="flex gap-16 items-center">
                 <div id="Logo " className="flex items-center">
-                    <span className="hidden md:flex"><Logo /></span>
-                    <span className=" md:hidden"> <LogoSm /></span>
+                    <span className="hidden md:flex px-1"><Logo /></span>
+                    <span className=" md:hidden z-40"> <LogoSm /></span>
                 </div>
-                <div className=" flex font-normal items-center text-sm text-[#090909] gap-5">
+                <div className=" hidden lg:flex font-light items-center text-sm text-[#090909] gap-5">
                     <p>Explore</p>
                     <p>Services</p>
                     <p>Individuals</p>
@@ -17,9 +22,27 @@ const NavBar = () => {
                     <p>FAQs</p>
                 </div>
             </div>
-            <div className="flex items-center">
-                <button className="px-8 py-3 border border-black rounded-full">Contact us</button>
-                <CircleArrow />
+            <div className="flex items-center relative">
+                <div className="flex  items-center pr-2">
+                    <button className="px-8 py-3 border-[1px] border-black  text-sm rounded-full">Contact us</button>
+                    <CircleArrow />
+                </div>
+                <span className="z-40 lg:hidden " onClick={() => { setShow(prev => !prev) }}>
+                    {
+                        show ? <Hamburger /> : <Hamburger />
+                    }
+
+                </span>
+                {
+                    show && <div className="bg-white text-2xl flex flex-col absolute top-0 h-screen gap-5 px-20 py-20 w-[100vw] right-0  lg:hidden">  <p>Explore</p>
+                        <p>Services</p>
+                        <p>Individuals</p>
+                        <p>Business</p>
+                        <p>Blog</p>
+                        <p>FAQs</p>
+                    </div>
+                }
+
             </div>
         </div>
     )
