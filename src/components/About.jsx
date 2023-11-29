ffde1b010796b4c8f6fa31225b59ec6ec50c31c7
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2023-11-29 17:28:45 +0100

    about

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div className="text-[40px] text-center leading-[60px] uppercase px-[150px] py-32">Our journey began 2013, 
+        <span className=""><img className="h-12 m-2 inline-block" src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1701274531/harvest%20Grove/Frame_18536_l6qxy1.png" /></span>when a group
+            of dedicated farmers set out to create a farm.
+            <span><img className="h-12 inline-block m-2" src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1701274529/harvest%20Grove/Frame_18537_bz2lmi.png" /> </span>that would not only yield bountiful harvests but also nourish the community. </div>
+    )
+}
+
+export default About
