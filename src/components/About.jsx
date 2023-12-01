28f26eb4e2125c56197679293ad495c9562649df
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2023-12-01 13:27:57 +0100

    about their goals

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,34 @@
-
 const About = () => {
-    return (
-        <div className="text-[40px] text-center leading-[60px] uppercase px-[150px] py-32">Our journey began 2013, 
-        <span className=""><img className="h-12 m-2 inline-block" src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1701274531/harvest%20Grove/Frame_18536_l6qxy1.png" /></span>when a group
-            of dedicated farmers set out to create a farm.
-            <span><img className="h-12 inline-block m-2" src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1701274529/harvest%20Grove/Frame_18537_bz2lmi.png" /> </span>that would not only yield bountiful harvests but also nourish the community. </div>
-    )
-}
+  return (
+    <div className="md:text-[38px] font-light md:font-normal text-[22px] text-center md:leading-[50px] uppercase px-2 md:px-[150px] py-20 md:py-32">
+      Our journey began 2013,
+      <span className="">
+        <img
+          className="rounded-full m-1 h-6 inline-block  md:hidden"
+          src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1701361078/harvest%20Grove/happy-cheerful-african-american-farm-worker-holding-crate-full-local-eco-friendly-ripe-leafy-greens-from-sustainable-crop-harvest-entrepreneurial-bio-permaculture-greenhouse-farm_lhw9gu.png"
+          alt=""
+        />
+        <img
+          className="hidden md:h-12 m-2 md:inline-block"
+          src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1701274531/harvest%20Grove/Frame_18536_l6qxy1.png"
+        />
+      </span>
+      when a group of dedicated farmers set out to create a farm
+      <span>
+        <img
+          className="rounded-full m-0.5 mb-1 h-6 inline-block md:hidden"
+          src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1701361077/harvest%20Grove/front-view-woman-carrying-harvest_1_ffuy2b.png"
+          alt=""
+        />
+        <img
+          className="hidden md:h-12 md:inline-block m-2"
+          src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1701274529/harvest%20Grove/Frame_18537_bz2lmi.png"
+        />
+      </span>
+      that would not only yield bountiful harvests but also nourish the
+      community.
+    </div>
+  );
+};
 
-export default About
+export default About;
