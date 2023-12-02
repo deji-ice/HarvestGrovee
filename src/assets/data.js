4b89894b7bca888fe438267df8284f3bb9545c7c
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2023-12-02 21:16:16 +0100

    crreated data file

---
+++ src/assets/data.js
@@ -0,0 +1,23 @@
+export const arrayOfObjects = [
+  {
+    title: "Premium Produce",
+    description:
+      "Our diverse range of crops, from succulent fruits to crisp vegetables, showcases the fertile abundance of our land.",
+    image:
+      "https://res.cloudinary.com/dhvwthnzq/image/upload/v1701547653/harvest%20Grove/countryside-woman-holding-plant-leaves_1_umqj98.png",
+  },
+  {
+    title: "Sustainable Farming",
+    description:
+      "We take pride in our eco-friendly farming practices that prioritize soil health and environmental sustainability.",
+    image:
+      "https://res.cloudinary.com/dhvwthnzq/image/upload/v1701547610/harvest%20Grove/happy-cheerful-african-american-farm-worker-holding-crate-full-local-eco-friendly-ripe-leafy-greens-from-sustainable-crop-harvest-entrepreneurial-bio-permaculture-greenhouse-farm_1_k6coil.png",
+  },
+  {
+    title: "Farm-to-Table Experience",
+    description:
+      "We offer a unique farm-to-table experience. Visit our farm, pick your own produce, & enjoy the farm-fresh taste that's second to none.",
+    image:
+      "https://res.cloudinary.com/dhvwthnzq/image/upload/v1701547697/harvest%20Grove/front-view-woman-carrying-harvest_1_1_jrq5jn.png",
+  },
+];
