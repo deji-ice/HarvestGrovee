7264ef514c0cff8537dfa6b226dd8d342fd6c8ba
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-01-23 10:50:38 +0100

    desktop design

--- src/components/CultivateSustain.jsx
+++ src/components/CultivateSustain.jsx
@@ -3,7 +3,7 @@ import { CircleArrow } from "../assets";
 const CultivateSustain = () => {
   return (
     <div className="relative px-5 lg:px-14 mt-14  xl:mt-28">
-      <p className="text-[27px] xl:text-[37px] xl:top-0 xl:leading-normal  xl:left-[64px] md:absolute md:text-2xl lg:text-3xl lg:top-1 lg:left-6 ">
+      <p className="text-[27px] lg:text-[29px] xl:text-[37px] xl:top-0 xl:leading-normal  xl:left-[64px] md:absolute md:text-2xl lg:top-1 lg:left-14 ">
         Cultivating Sustainability <br /> Agriculture
       </p>
       <img
@@ -16,7 +16,7 @@ const CultivateSustain = () => {
         src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1705758215/Subtract_8_wjyib0.png"
         alt=""
       />
-      <div className="flex absolute z-30 right-5 bottom-[-15.5rem] justify-end mt-2 lg:hidden  items-center pr-2">
+      <div className="flex absolute z-30 right-5 bottom-[-15.5rem] justify-end mt-2 lg:top-5 xl:top-36 lg:right-20  items-center pr-2">
         <button className="px-8  bg-white   py-3 border-[1px]  text-sm rounded-full">
           Contact us
         </button>
