58b1da3b800f9924c0fc4afb8d7373571fb07429
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-01-23 14:25:42 +0100

    touches

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import About from './components/About'
 import Cultivate from './components/Cultivate'
 import CultivateSustain from './components/CultivateSustain'
 import Farm from './components/Farm'
+import Footer from './components/Footer'
 import HeroSection from './components/HeroSection'
 import NavBar from './components/NavBar'
 import Provide from './components/Provide'
@@ -18,6 +19,7 @@ function App() {
     <Provide/>
     <Cultivate/>
     <CultivateSustain/>
+    <Footer/>
     {/* <div>Icons made from <a href="https://www.onlinewebfonts.com/icon">svg icons</a>is licensed by CC BY 4.0</div> */}
     </>
   )
